6830394e96fa0c4a64339992810f58df36b6ec18
micahyap <yapmicahsy@gmail.com>
2024-10-28 23:16:13 +0800

    add(Routes): added custom routes for login, signup, and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  post 'login', to: 'users#login'
+  post 'signup', to: 'users#create'
+  delete 'logout', to: 'user#logout'
+
   resources :users do
     resources :expenses
   end
