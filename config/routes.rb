d424e512efd24ab391423c81a6df565054fd27fe
micahyap <yapmicahsy@gmail.com>
2024-10-29 22:35:16 +0800

    fix(Route): fixed typo on custom logout route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   post 'login', to: 'users#login'
   post 'signup', to: 'users#create'
-  delete 'logout', to: 'user#logout'
+  delete 'logout', to: 'users#logout'
 
   resources :users do
     resources :expenses
